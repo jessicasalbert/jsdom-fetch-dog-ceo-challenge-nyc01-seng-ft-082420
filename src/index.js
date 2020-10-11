7d6ed3720e5ae65c5dedfc153312f98e1bba2bd4
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-11 10:38:31 -0400

    completed deliverable 2 with weird list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-console.log('%c HI', 'color: firebrick')
 
 document.addEventListener("DOMContentLoaded", () => {
+    fetchImages()
+    fetchBreeds()
+})
+
+function fetchImages() {
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then( res => res.json() )
     .then( imgs => renderImgs(imgs["message"]) )
     .catch( error => console.log(error.message))
-})
+}
 
 function renderImgs(array) {
     for (const img of array) {
@@ -16,8 +20,38 @@ function renderImgs(array) {
 function renderImg(img) {
     const picDiv = document.querySelector("#dog-image-container")
     const imag = document.createElement("img")
-    console.log(imag)
-    console.log(picDiv)
     imag.src = img
     picDiv.append(imag)
+}
+
+
+
+function getBreeds(breedObj) {
+    const breeds = []
+    for (const breed in breedObj) {
+        if (breedObj[breed] === "[]") {
+            breeds.push(breed)
+        } else {
+            for (let i = 0; i < breedObj[breed].length; i++) {
+                breeds.push(`${breedObj[breed][i]} ${breed}`)
+            }
+        }
+    }
+    return breeds
+}
+
+function renderBreeds(array) {
+    const dogUl = document.querySelector("#dog-breeds")
+    for (breed of array) {
+        const breedLi = document.createElement("li.breed")
+        breedLi.textContent = breed
+        dogUl.append(breedLi)
+    }
+}
+
+function fetchBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then( res => res.json() )
+    .then(breeds => renderBreeds(getBreeds(breeds['message'])))
+    .catch( error => console.log(error.message))
 }
