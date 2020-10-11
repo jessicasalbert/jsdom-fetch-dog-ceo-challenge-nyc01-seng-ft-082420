3c8a312a4abb1f60d468d56763b183d1e932e75a
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-10 23:43:19 -0400

    completed first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then( res => res.json() )
+    .then( imgs => renderImgs(imgs["message"]) )
+    .catch( error => console.log(error.message))
+})
+
+function renderImgs(array) {
+    for (const img of array) {
+        renderImg(img)
+    }
+}
+
+function renderImg(img) {
+    const picDiv = document.querySelector("#dog-image-container")
+    const imag = document.createElement("img")
+    console.log(imag)
+    console.log(picDiv)
+    imag.src = img
+    picDiv.append(imag)
+}
